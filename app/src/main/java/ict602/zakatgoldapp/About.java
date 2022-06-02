576b53e2845c487ee94fffd153fbf116f223e49c
putera34 <106551828+putera34@users.noreply.github.com>
2022-06-03 00:09:47 +0800

    Update About.java

--- app/src/main/java/ict602/zakatgoldapp/About.java
+++ app/src/main/java/ict602/zakatgoldapp/About.java
@@ -10,5 +10,8 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        
+        TextView text = findViewById(R.id.textViewLink);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
